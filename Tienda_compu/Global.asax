f931c79cdcaee34d751678ecd45d56064d3d8a8f
draenwing <draenwing@gmail.com>
2016-12-01 11:17:04 +0800

    proyecto

---
+++ Tienda_compu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tienda_compu.MvcApplication" Language="C#" %>
---
+++ Tienda_compu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tienda_compu.Startup))]
+namespace Tienda_compu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
