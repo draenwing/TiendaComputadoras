4ba51ec1aa80b963f053372e857600ec08da0439
draenwing <draenwing@gmail.com>
2016-12-05 08:11:34 +0800

    Notaciones en Producto

---
+++ Tienda_compu/Migrations/StoreContex/201612042358125_EditProduct.cs
@@ -0,0 +1,28 @@
+namespace Tienda_compu.Migrations.StoreContex
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EditProduct : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Productoes", "Nombre", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Productoes", "Marca", c => c.String(nullable: false, maxLength: 60));
+            AlterColumn("dbo.Productoes", "Ram", c => c.String(nullable: false, maxLength: 70));
+            AlterColumn("dbo.Productoes", "Almacenamiento", c => c.String(nullable: false, maxLength: 70));
+            AlterColumn("dbo.Productoes", "Peso", c => c.String(nullable: false, maxLength: 20));
+            AlterColumn("dbo.Productoes", "TipoPantalla", c => c.String(nullable: false, maxLength: 70));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Productoes", "TipoPantalla", c => c.String());
+            AlterColumn("dbo.Productoes", "Peso", c => c.String());
+            AlterColumn("dbo.Productoes", "Almacenamiento", c => c.String());
+            AlterColumn("dbo.Productoes", "Ram", c => c.String());
+            AlterColumn("dbo.Productoes", "Marca", c => c.String());
+            AlterColumn("dbo.Productoes", "Nombre", c => c.String());
+        }
+    }
+}
--- Tienda_compu/Models/Producto.cs
+++ Tienda_compu/Models/Producto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,14 +10,46 @@ namespace Tienda_compu.Models
     public class Producto
     {
         public int Id { get; set; }
+
+        [DisplayName("Nombre")]
+        [Required(ErrorMessage = "Es exigido ingresar el nombre de la PC")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre de la PC no puede ser mayor a 50 caracteres")]
         public String Nombre { get; set; }
+
+        [DisplayName("Marca del PC")]
+        [Required(ErrorMessage = "Es requido ingresar la marca de la PC")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "La marca de la PC no puede ser mayor a 60 caracteres")]
         public String Marca { get; set; }
+
+        [DisplayName("Memoria Ram")]
+        [Required(ErrorMessage = "Es requido ingresar los valores de la RAM de la PC")]
+        [StringLength(70, MinimumLength = 3, ErrorMessage = "La descripcion de la mmoria RAM no puede ser mayor a 70 caracteres")]
         public string Ram { get; set; }
+
+        [DisplayName("Almacenamiento de la PC")]
+        [Required(ErrorMessage = "Es requido ingresar las caracteristicas de almacenamiento de la PC")]
+        [StringLength(70, MinimumLength = 3, ErrorMessage = "El almacenamiento de la PC no puede ser mayor a 70 caracteres")]
         public string Almacenamiento { get; set; }
+
+        [DisplayName("Peso de la PC")]
+        [Required(ErrorMessage = "Es requido ingresar el peso de la PC")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "el peso no debe ser mayo a 20 caracteres")]
         public string Peso { get; set; }
+
+        [DisplayName("Precio del PC")]
+        [Required(ErrorMessage = "Es requido ingresar el precio de la PC")]
+        //[StringLength(10, MinimumLength = 5, ErrorMessage = "El precio de la PC no puede ser mayor a los 10 caracteres")]
         public decimal Precio { get; set; }
+
+
         public string Color { get; set; }
+
+
         public String Teclado { get; set; }
+
+        [DisplayName("Tipo de pantalla de la PC")]
+        [Required(ErrorMessage = "Es requido ingresar la descripcion de la pantalla de la PC")]
+        [StringLength(70, MinimumLength = 3, ErrorMessage = "La descripcion de la pantalla de la PC no puede ser mayor a 70 caracteres")]
         public string TipoPantalla { get; set; }
 
         public int? CategoriaId { get; set; }
