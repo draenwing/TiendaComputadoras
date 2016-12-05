08e7aa36627bea5820a823b21caecc7e8aa4b2c9
HackKiller1706 <somarribasaul@gmail.com>
2016-12-05 14:30:21 -0600

    yeah

--- Tienda_compu/App_Start/BundleConfig.cs
+++ Tienda_compu/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace Tienda_compu
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/fade").Include(
+                      "~/Scripts/fade.js"));
+
         }
     }
 }
